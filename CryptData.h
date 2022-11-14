5cb9136384b4edb83be31494f00e71bee1fc58fd
Iizerd <47404005+Iizerd@users.noreply.github.com>
2022-11-13 23:36:40 -0800

    backup

--- CryptData.h
+++ CryptData.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 #define __CD_U8 unsigned char
 #define __CD_U16 unsigned short
 #define __CD_U32 unsigned int
@@ -45,52 +47,120 @@ consteval __CD_U64 __CD_Hash64(const __CD_U32 Seed)
 	return (((__CD_U64)__CD_Hash32<Data_name>(Seed)) | (((__CD_U64)__CD_Hash32<Data_name>(Seed * 2)) << 32));
 }
 
+template<__CD_U8> struct __CD_DATA_STORAGE;
+template<> struct __CD_DATA_STORAGE<1> { using Type = __CD_U8; };
+template<> struct __CD_DATA_STORAGE<2> { using Type = __CD_U16; };
+template<> struct __CD_DATA_STORAGE<4> { using Type = __CD_U32; };
+template<> struct __CD_DATA_STORAGE<8> { using Type = __CD_U64; };
+
+// Every type that can be coerced into an 8 byte value and is returned in gpr
+template<__CD_U64 Hash_value, typename> struct __CD_CRYPT_SHELLCODE
+{
+	constexpr static std::array<__CD_U8, 37> Data =
+	{
+		0x0F, 0x31,												/*rdtsc*/
+		0x48, 0x01, 0x05, 0x0B, 0x00, 0x00, 0x00,				/*add [hash],rax*/
+		0x66, 0xC7, 0x05, 0xEE, 0xFF, 0xFF, 0xFF, 0xEB, 0x10,	/*mov start, disp to past setup*/
+		0x48, 0xB8, 											/*movabs rax,hash*/
+		((0xFFull << 0x0) & Hash_value >> 0x0),					/*hash*/
+		(((0xFFull << 0x8) & Hash_value) >> 0x8),
+		(((0xFFull << 0x10) & Hash_value) >> 0x10),
+		(((0xFFull << 0x18) & Hash_value) >> 0x18),
+		(((0xFFull << 0x20) & Hash_value) >> 0x20),
+		(((0xFFull << 0x28) & Hash_value) >> 0x28),
+		(((0xFFull << 0x30) & Hash_value) >> 0x30),
+		(((0xFFull << 0x38) & Hash_value) >> 0x38),
+		0x48, 0x31, 0xC8, 										/*xor rax,rcx*/
+		0xC3,
+		0x90, 0x90, 0x90, 0x90, 0x90							/*padding so same size*/
+	};
+};
+// Float specialization
+template<__CD_U64 Hash_value> struct __CD_CRYPT_SHELLCODE<Hash_value, float>
+{
+	constexpr static std::array<__CD_U8, 37> Data = {
+			0x0F, 0x31,												/*rdtsc*/
+			0x48, 0x01, 0x05, 0x0B, 0x00, 0x00, 0x00,				/*add [hash],rax*/
+			0x66, 0xC7, 0x05, 0xEE, 0xFF, 0xFF, 0xFF, 0xEB, 0x10,	/*mov start, disp to past setup*/
+			0x48, 0xB8, 											/*movabs rax,hash*/
+			((0xFFull << 0x0) & Hash_value >> 0x0),					/*hash*/
+			(((0xFFull << 0x8) & Hash_value) >> 0x8),
+			(((0xFFull << 0x10) & Hash_value) >> 0x10),
+			(((0xFFull << 0x18) & Hash_value) >> 0x18),
+			(((0xFFull << 0x20) & Hash_value) >> 0x20),
+			(((0xFFull << 0x28) & Hash_value) >> 0x28),
+			(((0xFFull << 0x30) & Hash_value) >> 0x30),
+			(((0xFFull << 0x38) & Hash_value) >> 0x38),
+			0x48, 0x31, 0xC8, 										/*xor rax,rcx*/
+			0x66, 0x48, 0x0f, 0x6e, 0xc0,							/*movq xmm0,rax*/
+			0xC3,
+	};
+};
+// Double specialization
+template<__CD_U64 Hash_value> struct __CD_CRYPT_SHELLCODE<Hash_value, double>
+{
+	constexpr static std::array<__CD_U8, 37> Data = __CD_CRYPT_SHELLCODE<Hash_value, float>::Data;
+};
+
+#define __CDDS(__Type) __CD_DATA_STORAGE<sizeof(__Type)>::Type
+
+#define __COERCE_TYPE(__Val) (*(typename __CD_DATA_STORAGE<sizeof(decltype(__Val))>::Type*)&__Val)
 
 #define CD_DEFINE(__Type, __Name)																															\
 static_assert(sizeof(__Type) <= 8, "Invalid type size.");																									\
 __Type __Cd_np_##__Name;																																	\
-constexpr static __CD_U64 __Cd_np_##__Name##_seed = __CD_Hash64<#__Name>(1776)/* + __LINE__ + __TIME__[7] + __TIME__[6] + __TIME__[4]*/;					\
-__declspec(allocate(".cdata")) inline static __CD_U8 __Cd_np_crypt_##__Name[] = {																			\
-		0x0F, 0x31,												/*rdtsc*/																					\
-		0x48, 0x01, 0x05, 0x0B, 0x00, 0x00, 0x00,				/*add [hash],rax*/																			\
-		0x66, 0xC7, 0x05, 0xEE, 0xFF, 0xFF, 0xFF, 0xEB, 0x10,	/*mov start, disp to past setup*/															\
-		0x48, 0xB8, 																																		\
-		((0xFFull << 0x0)& __Cd_np_##__Name##_seed),																										\
-		(((0xFFull << 0x8)& __Cd_np_##__Name##_seed) >> 0x8), 																								\
-		(((0xFFull << 0x10)& __Cd_np_##__Name##_seed) >> 0x10), 																							\
-		(((0xFFull << 0x18)& __Cd_np_##__Name##_seed) >> 0x18), 																							\
-		(((0xFFull << 0x20)& __Cd_np_##__Name##_seed) >> 0x20), 																							\
-		(((0xFFull << 0x28) & __Cd_np_##__Name##_seed) >> 0x28), 																							\
-		(((0xFFull << 0x30)& __Cd_np_##__Name##_seed) >> 0x30), 																							\
-		(((0xFFull << 0x38)& __Cd_np_##__Name##_seed) >> 0x38), 																							\
-		0x48, 0x31, 0xC8, 										/*xor rax,rcx*/																				\
-		0xC3																																				\
-};																																							\
+constexpr static __CD_U64 __Cd_np_##__Name##_hash = __CD_Hash64<#__Name>(1776)/* + __LINE__ + __TIME__[7] + __TIME__[6] + __TIME__[4]*/;					\
+__declspec(allocate(".cdata")) constexpr static auto __Cd_np_crypt_##__Name = __CD_CRYPT_SHELLCODE<__Cd_np_##__Name##_hash, __Type>::Data;					\
 																																							\
 																																							\
 __forceinline const __Type Get##__Name() const																												\
 {																																							\
-	return ((__Type(*)(__Type))(&__Cd_np_crypt_##__Name))(__Cd_np_##__Name);																				\
+	return ((__Type(*)(__CDDS(__Type)))(__Cd_np_crypt_##__Name.data()))(__COERCE_TYPE(__Cd_np_##__Name));															\
 }																																							\
-__forceinline void Set##__Name(__Type const __Val)																											\
+__forceinline void Set##__Name(__Type __Val)																												\
 {																																							\
-	__Cd_np_##__Name = ((__Type(*)(__Type))(&__Cd_np_crypt_##__Name))(__Val);																				\
+	__Cd_np_##__Name = ((__Type(*)(__CDDS(__Type)))(__Cd_np_crypt_##__Name.data()))(__COERCE_TYPE(__Val));														\
 }
 
 #define CD_MAKE_INITIALIZER(__Name, __Val)																													\
-__Cd_np_##__Name(((decltype(__Cd_np_##__Name)(*)(decltype(__Cd_np_##__Name)))(&__Cd_np_crypt_##__Name))(__Val))
+	__Cd_np_##__Name(((decltype(__Cd_np_##__Name)(*)(__CDDS(decltype(__Cd_np_##__Name))))(&__Cd_np_crypt_##__Name))(__COERCE_TYPE(__Val)))
 
 
-
+//#define CD_DEFINE(__Type, __Name)																															\
+//static_assert(sizeof(__Type) <= 8, "Invalid type size.");																									\
+//__Type __Cd_np_##__Name;																																	\
+//constexpr static __CD_U64 __Cd_np_##__Name##_hash = __CD_Hash64<#__Name>(1776)/* + __LINE__ + __TIME__[7] + __TIME__[6] + __TIME__[4]*/;					\
+//__declspec(allocate(".cdata")) inline static __CD_U8 __Cd_np_crypt_##__Name[] = {																			\
+//		0x0F, 0x31,												/*rdtsc*/																					\
+//		0x48, 0x01, 0x05, 0x0B, 0x00, 0x00, 0x00,				/*add [hash],rax*/																			\
+//		0x66, 0xC7, 0x05, 0xEE, 0xFF, 0xFF, 0xFF, 0xEB, 0x10,	/*mov start, disp to past setup*/															\
+//		0x48, 0xB8, 											/*movabs rax,hash*/																			\
+//		((0xFFull << 0x0) & __Cd_np_##__Name##_hash >> 0x0),	/*hash*/																					\
+//		(((0xFFull << 0x8) & __Cd_np_##__Name##_hash) >> 0x8), 																								\
+//		(((0xFFull << 0x10) & __Cd_np_##__Name##_hash) >> 0x10), 																							\
+//		(((0xFFull << 0x18) & __Cd_np_##__Name##_hash) >> 0x18), 																							\
+//		(((0xFFull << 0x20) & __Cd_np_##__Name##_hash) >> 0x20), 																							\
+//		(((0xFFull << 0x28) & __Cd_np_##__Name##_hash) >> 0x28), 																							\
+//		(((0xFFull << 0x30) & __Cd_np_##__Name##_hash) >> 0x30), 																							\
+//		(((0xFFull << 0x38) & __Cd_np_##__Name##_hash) >> 0x38), 																							\
+//		0x48, 0x31, 0xC8, 										/*xor rax,rcx*/																				\
+//		0xC3																																				\
+//};																																							\
+//																																							\
+//																																							\
+//__forceinline const __Type Get##__Name() const																												\
+//{																																							\
+//	return ((__Type(*)(__CDDS(__Type)))(&__Cd_np_crypt_##__Name))(__COERCE_TYPE(__Cd_np_##__Name));															\
+//}																																							\
+//__forceinline void Set##__Name(__Type const __Val)																											\
+//{																																							\
+//	__Cd_np_##__Name = ((__Type(*)(__CDDS(__Type)))(&__Cd_np_crypt_##__Name))(__COERCE_TYPE(__Val));														\
+//}
 
 //OLD STUFF
 /*
 
-template<__CD_U8> struct __CD_DATA_STORAGE;
-template<> struct __CD_DATA_STORAGE<1> { using Type = __CD_U8;  };
-template<> struct __CD_DATA_STORAGE<2> { using Type = __CD_U16; };
-template<> struct __CD_DATA_STORAGE<4> { using Type = __CD_U32; };
-template<> struct __CD_DATA_STORAGE<8> { using Type = __CD_U64; };
+
 
 template<typename Data_type, __CD_U8 Data_size, __CD_U64 Crypt_hash>
 __forceinline Data_type __CD_Decrypt(Data_type Data)
--- main.cpp
+++ main.cpp
@@ -3,22 +3,55 @@
 #include <cstdio>
 
 
+//struct TEST_STRUCT
+//{
+//	CD_DEFINE(void*, Temp);
+//	
+//	TEST_STRUCT(void* startval)
+//		: CD_MAKE_INITIALIZER(Temp, startval)
+//	{ }
+//};
+
 struct TEST_STRUCT
 {
-	CD_DEFINE(void*, Temp);
-	
-	TEST_STRUCT(void* startval)
+	CD_DEFINE(double, Temp);
+
+	TEST_STRUCT(double startval)
 		: CD_MAKE_INITIALIZER(Temp, startval)
 	{ }
 };
 
 
+//unsigned char meme[] = { 0xFF };
+//
+//__forceinline const float __Name(float value)
+//{
+//	typedef __CD_DATA_STORAGE<4>::Type(*memefn)(__CD_DATA_STORAGE<4>::Type);
+//
+//	//return ((float(*)(__CDDS(float)))(&meme))(__COERCE_TYPE(value));
+//	return ((float(*)(__CDDS(float)))(&meme))(__COERCE_TYPE(meme));
+//}
+
+
 int main()
 {
-	auto Ts = new TEST_STRUCT((void*)0xFFABCDEF);
-	printf("Temp:    %p\n", Ts->GetTemp());
-	printf("crypted: %p\n", Ts->__Cd_np_Temp);
-	Ts->SetTemp((void*)0xFFAAFFAAFFAAFFAA);
-	printf("Temp:    %p\n", Ts->GetTemp());
-	printf("crypted: %p\n", Ts->__Cd_np_Temp);
+
+	//constexpr auto meme = __CD_CRYPT_SHELLCODE<1776, int>::Data;
+
+	//auto Ts = new TEST_STRUCT((void*)0x1000231);
+	////Ts->SetTemp((void*)0x1992);
+	//printf("Temp:    %p\n", Ts->GetTemp());
+	//printf("crypted: %p\n", Ts->__Cd_np_Temp);
+	//Ts->SetTemp((void*)0x1776);
+	//printf("Temp:    %p\n", Ts->GetTemp());
+	//printf("crypted: %p\n", Ts->__Cd_np_Temp);
+
+
+	auto Ts = new TEST_STRUCT(1.2);
+	printf("Temp:    %.3f\n", Ts->GetTemp());
+	printf("crypted: %.3f\n", Ts->__Cd_np_Temp);
+	Ts->SetTemp(1.52312);
+	printf("Temp:    %.3f\n", Ts->GetTemp());
+	printf("crypted: %.3f\n", Ts->__Cd_np_Temp);
+
 }
